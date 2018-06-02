1037ab1e83ed9761388b0c28fc34d4de0d9848de
bac3phi <gikizay@gmail.com>
2018-06-02 16:37:22 +0700

    SearchView

--- src/CM/Controllers/BillSearchController.java
+++ src/CM/Controllers/BillSearchController.java
@@ -1,7 +1,70 @@
 package CM.Controllers;
 
 
-public class BillSearchController  {
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXRadioButton;
+import com.jfoenix.controls.JFXTextArea;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class BillSearchController implements Initializable {
+    @FXML
+    private JFXButton btnPRINT;
+
+    @FXML
+    private JFXButton btnSEARCH;
+
+    @FXML
+    private JFXButton btnGETINFO;
+
+    @FXML
+    private JFXButton btnREFRESH;
+
+    @FXML
+    private JFXTextArea txtSEARCH;
+
+    @FXML
+    private JFXRadioButton rdbtnBillID;
+
+    @FXML
+    private JFXRadioButton rdbtnCustomerID;
+
+    @FXML
+    private JFXRadioButton rdbtnEmployeesID;
+
+    @FXML
+    private JFXDatePicker dateCheckIn;
+
+    @FXML
+    private JFXDatePicker dateCheckOut;
+
+    @FXML
+    void setBtnGETINFO(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setBtnPRINT(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setBtnREFRESH(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setBtnSEARCH(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
